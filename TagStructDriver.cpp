8e7ce23aef17c370ba2f85dd8801dcabfd8f202c
Cameron Misplon <mspcam001@nightmare.cs.uct.ac.za>
2021-03-24 16:49:19 +0200

    Completed main method as well as fixing error with multiple definitions of function

---
+++ TagStructDriver.cpp
@@ -0,0 +1,48 @@
+#include "TagStruct.h"
+#include <string>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::string message = "r: Read and process tag file \np: Print all tags \nd: Write data to tag.txt \nl: Print tag data for a tag you enter \nq: Quit \nEnter an option (r,p,d,l) or q to quit, and press return.";
+	std::string option;
+	std::string fileName;
+        std::string chosenTag;
+	std::string returnKey;
+	for(;;)
+	{
+		std::cout << message << std::endl;
+		std::cin >> option;
+		if (option=="q")
+		{
+			MSPCAM001::message_Clear();
+			break;
+		}
+		else if (option=="r")
+		{
+			std::cout << "Enter text file name (with .txt), and press return" << std::endl;
+			MSPCAM001::tag_Clear();
+			std::cin >> fileName;
+			MSPCAM001::fileread(fileName);
+		}
+		else if (option=="p")
+		{
+			MSPCAM001::print_Tags();
+		}
+		else if (option=="d")
+		{
+			MSPCAM001::write_Tags();
+		}
+		else if (option=="l")
+		{
+			std::cout << "Please enter your desired tag:" << std::endl;
+			std::cin >> chosenTag;
+			MSPCAM001::single_Tag(chosenTag);
+		}
+		std::cout << "Enter 'next' to re-display menu:" << std::endl;
+		std::cin >> returnKey;
+		MSPCAM001::message_Clear();
+	}
+	return 0;
+}
+
